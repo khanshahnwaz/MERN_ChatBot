49e7e1c1f214954c917d43572b4723af0e66b483
khanshahnwaz <shahnwazkhan2007@gmail.com>
2024-11-10 11:20:08 +0530

    node and express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 import express from 'express'
 // app holds the functionality of express
 const app=express();
+// middlewwares
+app.use(express.json())
 
+
+// connections and listeners
 app.listen(5000,()=>console.log("Server running 5000"))
+
