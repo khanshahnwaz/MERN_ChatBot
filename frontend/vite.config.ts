d9a67051fedaeccc2235e7a761aca27d4ef59fad
khanshahnwaz <shahnwazkhan2007@gmail.com>
2025-04-29 01:57:20 +0530

    build the deploying

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vite config
 export default defineConfig({
-  // base:"MERN_ChatBot_Frontend",
   plugins: [react()],
   optimizeDeps: {
-    include: ['react', 'react-dom'], // Ensure react and react-dom are included for optimization
+    include: ['react', 'react-dom'],
   },
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'], // Avoid marking react as external, ensure it's bundled
-    },
-  },
-  // resolve: {
-  //   alias: {
-  //     react: require.resolve('react'),
-  //     react-dom: require.resolve('react-dom')
-  //   }
-  // }
 });
